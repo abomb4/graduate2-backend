50766c4603194c0998cf0274a04e5703686e6a0f
yangrlx <yangrlx@gmail.com>
2018-05-06 21:37:14 +0800

    上传文件功能

---
+++ src/main/java/org/wlyyy/itrs/service/StorageService.java
@@ -0,0 +1,26 @@
+package org.wlyyy.itrs.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/**
+ * 上传文件功能的后端存储服务
+ */
+public interface StorageService {
+
+    void init();
+
+    String store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+
+}
